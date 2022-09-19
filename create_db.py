48233df541d9d9150504f41b7c84c67a6190f301
Danick Loic Yagoue <dy@alphabytes.de>
2022-09-19 21:33:17 +0200

    Delete a file

--- create_db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(
-    host="185.224.138.28",
-    user="u345760943_gpx_flask_proj",
-    passwd="@Test_laravel3194",
-)
-
-my_cursor = mydb.cursor()
